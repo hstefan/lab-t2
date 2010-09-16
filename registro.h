d73ae8d93fee4830c25ddf90e40917e1e0118ee2
Hugo Stefan Kaus Puhlmann <hugopuhlmann@gmail.com>
2010-09-16 11:02:07 -0300

    classe que deve ser herdada por cada elemento que faca uso do 'banco'

---
+++ registro.h
@@ -0,0 +1,17 @@
+#ifndef HUGO_REGISTRO_H__
+#define HUGO_REGISTRO_H__
+
+namespace data
+{
+	class Registro
+	{
+	public:
+		virtual void save() = 0;
+		virtual void erase() = 0;
+		virtual void sync() = 0;
+	private:
+		//TODO: db adapter singleton
+	};
+}
+
+#endif
