05438482e8f2f60a3e3bca7809a3e9db31a817e9
Hugo Stefan Kaus Puhlmann <hugopuhlmann@gmail.com>
2010-09-16 13:50:51 -0300

    declara as classes que serao usadas para fazer a interface de armazenamento de informacoes

---
+++ banco.h
@@ -0,0 +1,50 @@
+#ifndef HUGO_BANCO_H__
+#define HUGO_BANCO_H__
+
+#include "registro.h"
+#include "list.h"
+#include <string>
+
+namespace bd
+{
+	using data::Registro;
+
+	class Tabela
+	{
+	public:
+		Tabela(const std::string table_name);
+		virtual void store(Registro* reg) = 0;
+		virtual void remove(Registro* reg) = 0;
+		virtual void update(Registro* reg) = 0;
+
+		const std::string& table_name() const;
+
+	private:
+		std::string name;
+	};
+
+	class Banco
+	{
+	public:
+		static Banco& getInstance()
+		{ return _instance; }
+
+		void registerTable(Tabela* table);
+		void deleteTable(const std::string& name);
+		void save(const std::string& table, Registro* reg);
+		void sync(const std::string& table, Registro* reg);
+		void erase(const std::string& table, Registro* reg);
+	private:
+		Banco() 
+		{ }
+		~Banco()
+		{ }
+		Banco(const Banco&);
+		Banco& operator=(const Banco&);
+
+		static Banco _instance;
+		ds::list<Tabela*> tabelas;
+	};
+}
+
+#endif
